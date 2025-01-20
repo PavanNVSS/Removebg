3c79faf4d576783769523ee8211ea9fb9a14f6a8
Pavan Sastry <93876062+PavanNVSS@users.noreply.github.com>
2025-01-20 13:54:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def remove_bg():
     except Exception as e:
         print(f"Error: {str(e)}")
         return jsonify({'error': str(e)}), 500
-
+        
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0",port=5002)
+    port = int(os.environ.get("PORT", 5000))  # Dynamically assign port
+    app.run(debug=True, host="0.0.0.0", port=port)
